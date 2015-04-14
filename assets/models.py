4928672fbaab2c4bfdf64e9833d7caf8009f11c5
ccgswa <gnolan@ccgs.wa.edu.au>
2015-04-14 16:47:50 +0800

    Enabled static files. Uninstalled South. Updated requirements.txt

--- assets/models.py
+++ assets/models.py
@@ -1,14 +1,29 @@
+import datetime
 from django.db import models
 
 # Create your models here.
 
 class Asset(models.Model):
     name = models.CharField(max_length=30, primary_key=True, unique=True)
-    serial = models.CharField(max_length= 50, unique=True)
-    owner = models.CharField(max_length=50)
-    purchase_date = models.DateField
-    created_date = models.DateTimeField
-    mac = models.CharField(max_length=50, unique=True)
-    wmac = models.CharField(max_length=50, unique=True)
-    bmac = models.CharField(max_length=50, unique=True)
-    active = models.BooleanField
+    manufacturer = models.CharField(max_length=50, null=True)
+    model = models.CharField(max_length=50, null=True)
+    serial = models.CharField(max_length=50, unique=True)
+
+    LOCATION_CHOICES = (
+        ('none', 'None'),
+        ('ccgs', 'CCGS: Main Campus'),
+        ('koor', 'Kooringal Campus'),
+        ('dmg', 'Damaged'),
+        ('disp', 'Disposed'),
+        ('lost', 'Lost or Stolen')
+    )
+    location = models.CharField(max_length=4, choices=LOCATION_CHOICES, default='none' )
+
+    owner = models.CharField(null=True, max_length=50)
+    purchase_date = models.DateField(null=True)
+    created_date = models.DateField(null=True)
+    mac = models.CharField(max_length=50, unique=True, null=True)
+    wmac = models.CharField(max_length=50, unique=True, null=True)
+    bmac = models.CharField(max_length=50, unique=True, null=True)
+    active = models.BooleanField(default=True)
+
