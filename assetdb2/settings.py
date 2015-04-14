cb5dd9b95535ffd4c0aedaa00f05b8fa57ba81a5
ccgswa <gnolan@ccgs.wa.edu.au>
2015-04-14 13:07:48 +0800

    Enabled static files. Uninstalled South. Updated requirements.txt

--- assetdb2/settings.py
+++ assetdb2/settings.py
@@ -27,15 +27,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS  = {
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-#   'south',
+    'django.contrib.staticfiles',
     'assetman'
-)
+}
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
--- assetman/admin.py
+++ assetman/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from assetman.models import Asset
 
 # Register your models here.
+
+admin.site.register(Asset)
