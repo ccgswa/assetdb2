6d170b0b126130e803e1a018e8dc6404ff81878b
ccgswa <gnolan@ccgs.wa.edu.au>
2015-04-17 23:15:47 +0800

    Added a new view and template for browsing assets. Hooked this template into the site root directory.

--- assets/urls.py
+++ assets/urls.py
@@ -1,1 +1,7 @@
 __author__ = 'gnolan'
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.browse),
+]
--- assets/views.py
+++ assets/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 #TODO Create a view for the Asset object. https://docs.djangoproject.com/en/1.8/intro/tutorial03/
 
+def browse(request):
+
+    return render(request,'assets/browse.html',{})
